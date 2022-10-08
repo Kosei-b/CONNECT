18bf393bacb0434221bec0ff43cdc7a4336b26b1
Kosei Ban <64409793+Kosei-b@users.noreply.github.com>
2022-10-07 17:13:41 -0700

    Upload Files

---
+++ CONNECT(CALL APP)/CONNECT_CALL_APP_App.swift
@@ -0,0 +1,17 @@
+//
+//  CONNECT_CALL_APP_App.swift
+//  CONNECT(CALL APP)
+//
+//  Created by Kosei Ban on 2022-10-07.
+//
+
+import SwiftUI
+
+@main
+struct CONNECT_CALL_APP_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
